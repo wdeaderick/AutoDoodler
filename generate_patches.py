1418238e938691a02be36a372f1c543a0faed98a
wdeaderick <willdeaderick@gmail.com>
2018-11-13 14:51:43 -0800

    Code to sample the doodle patches

---
+++ generate_patches.py
@@ -0,0 +1,27 @@
+import os.path
+from PIL import Image
+import numpy
+import math
+from sklearn.feature_extraction import image
+from sklearn.preprocessing import normalize
+import scipy.misc
+
+original_files = []
+## Get filenames
+for filename in os.listdir():
+    original_files.append(filename)
+
+## Extract patches and save them
+for filename in original_files:    
+    if filename[-3:] == "jpg" or filename[-3:] == "png":
+        im = Image.open(filename).convert('L')
+        (width, height) = im.size ##PIL convention width first
+        greyscale_map = list(im.getdata())
+        greyscale_map = numpy.array(greyscale_map)
+        greyscale_map = greyscale_map.reshape((height, width)) ##numpy height first
+        patches = image.extract_patches_2d(greyscale_map, (256, 256), max_patches = 500)
+        for image_ind in range(500):
+            curimage = patches[image_ind,:,:]
+            curimage = normalize(curimage)
+            name = filename + "patch" + str(image_ind) + ".jpg"
+            scipy.misc.imsave(name, curimage)
